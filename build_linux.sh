1d1505799399e9cd2933fad6d2955db2f0a0ef57
leaf <leaf@leaf.sx>
2019-11-19 09:01:41 +0000

    Change build flags (rm rtti,.eh_frame)

--- build_linux.sh
+++ build_linux.sh
@@ -8,9 +8,9 @@ cp crunch/inc/crn_defs.h crn_defs.h
 
 cp ../c_crunch_lib.cpp c_crunch_lib.cpp
 
-g++ c_crunch_lib.cpp -c -Os --std=gnu++0x -fomit-frame-pointer -ffast-math -fno-math-errno -fno-strict-aliasing -fPIC -g -Wall -Wno-unused-value -Wno-unused -march=core2 -shared -o libcrndecomp-linux.so
+g++ c_crunch_lib.cpp -c -Os --std=gnu++0x -fno-rtti -fno-exceptions -fno-asynchronous-unwind-tables -fomit-frame-pointer -ffast-math -fno-math-errno -fno-strict-aliasing -fPIC -g -Wall -Wno-unused-value -Wno-unused -march=core2 -shared -o libcrndecomp-linux.so
 
-g++ c_crunch_lib.cpp -c -Os --std=gnu++0x -fomit-frame-pointer -ffast-math -fno-math-errno -fno-strict-aliasing -g -Wall -Wno-unused-value -Wno-unused -march=core2 -o libcrndecomp-linux-static.o
+g++ c_crunch_lib.cpp -c -Os --std=gnu++0x -fno-rtti -fno-exceptions -fno-asynchronous-unwind-tables -fomit-frame-pointer -ffast-math -fno-math-errno -fno-strict-aliasing -g -Wall -Wno-unused-value -Wno-unused -march=core2 -o libcrndecomp-linux-static.o
 
 ar rcs libcrndecomp-linux-static.a libcrndecomp-linux-static.o
 
